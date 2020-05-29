a07b826605d009736f8349068737f729565a7485
Robin-LESAOUT <65037222+Robin-LESAOUT@users.noreply.github.com>
2020-05-29 21:51:13 +0200

    ajout du bouton delete

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -187,8 +187,15 @@ public class Controller {
 						createEllipse(event.getX(),event.getY(),arrayE).addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 							@Override
 							public void handle(MouseEvent event) {
-							arrayE.get(arrayE.size()-1).setRadiusX(arrayE.get(arrayE.size()-1).getRadiusX()+10);
-							arrayE.get(arrayE.size()-1).setRadiusY(arrayE.get(arrayE.size()-1).getRadiusY()+10);
+								if(model.isSelect()) {
+									boolean sel=false;
+									if(!sel) {
+										arrayE.get(arrayE.size()-1).setRadiusX(arrayE.get(arrayE.size()-1).getRadiusX()+10);
+										arrayE.get(arrayE.size()-1).setRadiusY(arrayE.get(arrayE.size()-1).getRadiusY()+10);
+										arrayE.get(arrayE.size()-1).setFill(colorPicker.getValue());
+										sel=true;
+									}
+								}
 							}
 						});
 						Pane.getChildren().add(arrayE.get(arrayE.size()-1));
@@ -199,8 +206,10 @@ public class Controller {
 						createRectangle(event.getX(),event.getY(),arrayR).addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 							@Override
 							public void handle(MouseEvent event) {
-							arrayR.get(arrayR.size()-1).setWidth(arrayR.get(arrayR.size()-1).getWidth()+10);
-							arrayR.get(arrayR.size()-1).setHeight(arrayR.get(arrayR.size()-1).getHeight()+10);
+								if(model.isSelect()) {
+									arrayR.get(arrayR.size()-1).setWidth(arrayR.get(arrayR.size()-1).getWidth()+10);
+									arrayR.get(arrayR.size()-1).setHeight(arrayR.get(arrayR.size()-1).getHeight()+10);
+								}
 							}
 						});
 						Pane.getChildren().add(arrayR.get(arrayR.size()-1));
@@ -216,23 +225,55 @@ public class Controller {
 				public void handle(MouseEvent event) {
 					if (model.isEl()) {
 						setEllipse(arrayE.get(arrayE.size()-1), event.getX() - (listeX.get(listeX.size()-1)), event.getY() - ((listeY.get(listeY.size()-1))));
+						arrayE.get(arrayE.size()-1).addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
+							@Override
+							public void handle(MouseEvent event) {
+								if(model.isSelect()) {
+									arrayE.get(arrayE.size()-1).setCenterX(event.getX());
+									arrayE.get(arrayE.size()-1).setCenterY(event.getY());
+								}
+							}
+						});
 					}else if(model.isRec()) {
 						setRectangle(arrayR.get(arrayR.size()-1),event.getX() - (listeX.get(listeX.size()-1)),event.getY()- ((listeY.get(listeY.size()-1))));
 					}
 				}
 			});
 		
-		////QuEstion 3
 		
+		deleteBtn.addEventHandler(MouseEvent.MOUSE_CLICKED,new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent event) {
+				if(model.isSelect()) {
+				Pane.getChildren().remove(arrayE.get(arrayE.size()-1));
+				}
+			}
+		});
+		
+		
+		
+		/*arrayE.get(arrayE.size()-1).addEventHandler(MouseEvent.MOUSE_RELEASED,new EventHandler<MouseEvent>(){
+						@Override
+						public void handle(MouseEvent event) {
+							arrayE.get(arrayE.size()-1).setRadiusX(arrayE.get(arrayE.size()-1).getRadiusX()-10);
+							arrayE.get(arrayE.size()-1).setRadiusY(arrayE.get(arrayE.size()-1).getRadiusY()-10);
+						}
+					});*/
+		
+
+		
+		////QuEstion 3
+		/*
 		Pane.addEventHandler(MouseEvent.MOUSE_PRESSED,new EventHandler<MouseEvent>(){
 			@Override
 			public void handle(MouseEvent event) {
 				if (model.isEl()) {
-					setEllipse(arrayE.get(arrayE.size()-1), event.getX() - (listeX.get(listeX.size()-1)), event.getY() - ((listeY.get(listeY.size()-1))));
+					//setEllipse(arrayE.get(arrayE.size()-1), event.getX() - (listeX.get(listeX.size()-1)), event.getY() - ((listeY.get(listeY.size()-1))));
 				}else if(model.isRec()) {
-					setRectangle(arrayR.get(arrayR.size()-1),event.getX() - (listeX.get(listeX.size()-1)),event.getY()- ((listeY.get(listeY.size()-1))));
+					//setRectangle(arrayR.get(arrayR.size()-1), event.getX() - (listeX.get(listeX.size()-1)),event.getY()- ((listeY.get(listeY.size()-1))));
 				}
 			}
 		});
+		*/
 	}
 }
